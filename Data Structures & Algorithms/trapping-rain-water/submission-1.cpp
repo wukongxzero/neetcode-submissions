35809f801d166847f25b892a5c55ef68224b6199
Pavan kushal Velagaleti <71929008+wukongxzero@users.noreply.github.com>
2026-07-27 12:37:04 -0400

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        if (height.empty()) {
+            return 0;
+        }
+        int n = height.size();
+        int res = 0;
+
+        for (int i = 0; i < n; i++) {
+            int leftMax = height[i];
+            int rightMax = height[i];
+
+            for (int j = 0; j < i; j++) {
+                leftMax = max(leftMax, height[j]);
+            }
+            for (int j = i + 1; j < n; j++) {
+                rightMax = max(rightMax, height[j]);
+            }
+
+            res += min(leftMax, rightMax) - height[i];
+        }
+        return res;
+    }
+};
